816b98b5e4a0355276bdefa3cb9ca3778587aa71
mohammadbapanah79 <mohammadbpnh979@gmail.com>
2025-11-13 22:17:51 +0330

    Selection sort

--- Test.php
+++ Test.php
@@ -1,33 +1,34 @@
 <?php
-$list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-    81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100
-];
 
-function binarySearch($list, $item)
+$list = [10, 15, 8, 2, 4, 3, 7, 12, 52, 6, 17];
+
+function findSmallest($arr)
 {
-    $low = 0;
-    $high = count($list) - 1;
-    $step = 0;
-    while ($low <= $high) {
-        $step++;
-        $mid = intval(($low + $high) / 2);
-        $guess = $list[$mid];
-        if ($guess == $item) {
-            echo "Step : " . $step . "<br/>";
-            return $mid;
-        } elseif ($guess > $item) {
-            $high = $mid - 1;
-        } else {
-            $low = $mid + 1;
+    $smallest = $arr[0];
+    $smallestIndex = 0;
+
+    for ($i = 0; $i < count($arr); $i++) {
+        if ($arr[$i] < $smallest) {
+            $smallest = $arr[$i];
+            $smallestIndex = $i;
         }
     }
-    echo "Step: " . $step . "<br/>";
-    return "Null!!";
+
+    return $smallestIndex;
 }
 
+function selectionSort($arr)
+{
+    $newArr = [];
+    $len = count($arr);
 
-echo "Index: " . binarySearch($list, 1111) . "<br/>";
+    for ($i = 0; $i < $len; $i++) {
+        $smallest = findSmallest($arr);
+        $newArr[] = $arr[$smallest];
+        array_splice($arr, $smallest, 1);
+    }
 
+    return $newArr;
+}
+
+print_r(selectionSort($list));
